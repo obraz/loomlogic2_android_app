37a8e70576567e7200f42d5feb68664914f20d47
alex <alex>
2017-04-14 12:23:13 +0300

    add test url scheme

--- app/src/main/java/com/loomlogic/Test.java
+++ app/src/main/java/com/loomlogic/Test.java
@@ -1,6 +1,9 @@
 package com.loomlogic;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.loomlogic.base.BaseActivity;
 
@@ -12,7 +15,9 @@ public class Test extends BaseActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        TextView tv = new TextView(this);
+        tv.setText("RESET PASSWORD");
+        addContentView(tv, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
     }
 
 
